f99e6a473608141c75f1e2a8e8fd7e2dad152164
sajith231 <sajiththomas231@gmail.com>
2025-11-04 13:58:15 +0530

    update

--- table_mapping.py
+++ table_mapping.py
@@ -5,8 +5,25 @@ TABLE_MAPPING = {
     },
     "batches": {
         "table": "acc_productbatch",
-        "columns": "productcode, cost, salesprice, bmrp, barcode, secondprice, thirdprice"
+        "columns": """
+            productcode, 
+            cost, 
+            salesprice, 
+            bmrp, 
+            barcode, 
+            secondprice, 
+            thirdprice, 
+            fourthprice,
+            (SELECT name FROM acc_pricecode WHERE code = 'CO') AS cost_name,
+            (SELECT name FROM acc_pricecode WHERE code = 'S1') AS sales_price_name,
+            (SELECT name FROM acc_pricecode WHERE code = 'MR') AS bmrp_name,
+            (SELECT name FROM acc_pricecode WHERE code = 'S2') AS secondprice_name,
+            (SELECT name FROM acc_pricecode WHERE code = 'S3') AS thirdprice_name,
+            (SELECT name FROM acc_pricecode WHERE code = 'S4') AS fourthprice_name
+        """
     },
+
+
     "customers": {
         "table": "acc_master",
         "columns": "code, name, super_code, address, phone, phone2",
